d9e8dafba3f8a26bcfc1cccf07b6c179aee30900
esoby <141106@naver.com>
2024-03-21 20:56:53 +0900

    ✨ 홈 페이지 컴포넌트 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import styled from "@emotion/styled";
+import Button from "../components/common/Button";
+import { useNavigate } from "react-router-dom";
+import PageTitle from "../components/common/PageTitle";
+
 const Home = () => {
-  return <div>TIC TAC TOE!</div>;
+  const navigate = useNavigate();
+
+  return (
+    <HomeContainer>
+      <PageTitle>TIC TAC TOE!</PageTitle>
+      <Button size="large" onClick={() => navigate("/setting")}>
+        게임 시작
+      </Button>
+      <Button size="large" onClick={() => navigate("/history")}>
+        기록된 게임 보기
+      </Button>
+    </HomeContainer>
+  );
 };
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
 
 export default Home;
