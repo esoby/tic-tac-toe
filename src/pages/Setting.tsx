b6adb2fdd037f7c20e2c8ed57b8349a495e67b82
esoby <141106@naver.com>
2024-03-22 10:47:00 +0900

    ✨ 게임 세팅 페이지 구현

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,5 +1,20 @@
+import styled from "@emotion/styled";
+import PageTitle from "../components/common/PageTitle";
+import SetupForm from "../components/Setting/SetupForm";
+
 const Setting = () => {
-  return <div>setting page</div>;
+  return (
+    <SettingContainer>
+      <PageTitle>- Game Setup -</PageTitle>
+      <SetupForm />
+    </SettingContainer>
+  );
 };
+
+const SettingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
 
 export default Setting;
